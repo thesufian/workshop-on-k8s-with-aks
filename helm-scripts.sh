0d11460fab0e5d3c97629fb35b713538a95cfd59
sufian <sufian@brainstation-23.com>
2020-01-11 12:25:02 +0600

    added installation walk-through script

--- helm-scripts.sh
+++
@@ -1,5 +0,0 @@
-# add official Helm stable charts
-helm repo add stable https://kubernetes-charts.storage.googleapis.com/
-helm repo update
-
-#helm install stable/nginx-ingress --set controller.replicaCount=2 --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux --set controller.service.loadBalancerIP="13.95.111.82" --namespace kube-system --generate-name
