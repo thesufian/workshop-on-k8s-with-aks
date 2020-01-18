5ec19dd7cd2fcf94798a685227813a8c56768e3b
sufian <sufian@brainstation-23.com>
2020-01-18 20:19:45 +0600

    updated ports

--- AKS/Services/HelloNode/server.js
+++ AKS/Services/HelloNode/server.js
@@ -1,6 +1,6 @@
 'use strict';
 var http = require('http');
-var port = process.env.PORT || 30002;
+var port = process.env.PORT || 80;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'application/json' });
