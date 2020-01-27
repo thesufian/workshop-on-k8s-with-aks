eec53d46966644007497d51fcfae07b8c6b22004
sufian <sufian@brainstation-23.com>
2020-01-27 18:44:28 +0600

    updated to include appsettings

---
+++ AKS/Services/HelloWorld/Api/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/hello")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public HelloController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            var message = _configuration["Hello:Message"];
+            return Ok(new { message });
+        }
+    }
+}
--- AKS/Services/HelloWorld/Program.cs
+++ AKS/Services/HelloWorld/Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace HelloWorld
 {
@@ -18,6 +13,12 @@ namespace HelloWorld
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((ctx, config) =>
+                {
+                    // (Optional) Add configuration file for Docker
+                    // - Note: This file should be deployed to Kubernetes as a secret
+                    config.AddJsonFile("settings/appsettings.docker.json", optional: true);
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
